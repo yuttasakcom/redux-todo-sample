e8a1c9e6b932dca6eaf6e5b1d1305d25d6d4fcc5
Yuttasak Pannawat <yuttasakcom@gmail.com>
2019-04-02 15:52:25 +0700

    udpated Users component

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
 import { setUsers } from "../store/users/action";
 
 class Users extends React.Component {
@@ -26,11 +25,7 @@ const mapStateToProps = state => {
   return { users: state.users };
 };
 
-const mapDispatchToProps = dispatch => {
-  return bindActionCreators({ setUsers }, dispatch);
-};
-
 export default connect(
   mapStateToProps,
-  mapDispatchToProps
+  { setUsers }
 )(Users);
